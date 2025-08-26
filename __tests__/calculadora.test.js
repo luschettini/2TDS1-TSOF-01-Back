2834e76192699db6886cb74c5618bfe64286c60e
Luiza <luizaschettinii@gmail.com>
2025-08-26 16:22:57 -0300

    feat: finish project

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar } from "../src/calculadora.js";
+import { somar,  subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -10,8 +10,21 @@ describe("Calculadora", () => {
 
     //Act (executar)
     const resultado = somar(a, b);
-    
+
     //Assert (verificar)
     expect(resultado).toBe(12);
 });
+
+    test("Deve subtrair dois números corretamente", () => {
+        // AAA
+        //Arrange (preparar)
+        const a = 5;
+        const b = 7;
+
+        //Act (executar)
+        const resultado = subtrair(a, b);
+
+        //Assert (verificar)
+        expect(resultado).toBe(-2);
+    });
 });
--- src/calculadora.js
+++ src/calculadora.js
@@ -2,3 +2,8 @@
 export function somar(a, b) {
     return a + b;
 }
+
+// Função para testar subtração
+export function subtrair(a, b) {
+    return a - b;
+}
